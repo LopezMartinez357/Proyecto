7d30401d168391dbc70064c5fd3b5cfc0a8490c5
LopezMartinez357 <sebastianlopezm2016@gmail.com>
2026-03-22 14:49:45 -0600

    Comienzo del codigo

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -7,4 +7,24 @@ class program
     static int rechazados = 0;
     static int revision = 0;
 
+    static void Main()
+    {
+
+        int opcion;
+
+        do
+        {
+            Console.WriteLine("MENU PRINCIPAL");
+            Console.WriteLine("1. Evaluar contenido");
+            Console.WriteLine("2. Mostrar reglas");
+            Console.WriteLine("3. Mostrar estadisticas");
+            Console.WriteLine("4. Reiniciar estadisticas");
+            Console.WriteLine("5. Salir");
+
+
+        }
+
+
+    }
+
 }
