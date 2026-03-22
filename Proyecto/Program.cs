7b775a4d9c0b7da46364a77edc13a7fd26c7feb3
LopezMartinez357 <sebastianlopezm2016@gmail.com>
2026-03-22 14:41:50 -0600

    Delcarar variables

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hola mundo");
+﻿using System;
+
+class program
+{
+    static int total = 0;
+    static int publicados = 0;
+    static int rechazados = 0;
+    static int revision = 0;
+
+}
