57e528aa3f364fab6152b05250ae96181400a708
LopezMartinez357 <sebastianlopezm2016@gmail.com>
2026-03-22 22:06:24 -0600

    Fin de las reglas

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 class program
 {
@@ -71,7 +72,40 @@ class program
 
         Console.WriteLine("Reglas del sistema");
         Console.WriteLine("Clasifiacion y horario:");
+        Console.WriteLine("Todo publico: cualquier hora");
+        Console.WriteLine("+13: 6:00 a 22:00");
+        Console.WriteLine("+18: 22:00 a 5:00");
+
+        Console.WriteLine("Duracion por tipo:");
+        Console.WriteLine("Peliculas: 60 - 180 minutos");
+        Console.WriteLine("Series: 20 - 90 minutos por episodio");
+        Console.WriteLine("Documentales: 30 - 120 minutos");
+        Console.WriteLine("Eventos en vivo: 30 - 240 minutos");
+
+        Console.WriteLine("Produccion");
+        Console.WriteLine("Baja: solo todo publico o +13");
+        Console.WriteLine("Media o Alta: cualquier clasificacion");
+
     }
 
+    static void  MostrarEstadisticas()
+    {
+        Console.WriteLine("Estadisticas del sistema");
+        Console.WriteLine("Total de contenidos evaluados: " + total);
+        Console.WriteLine("Publicados: " + publicados);
+        Console.WriteLine("Rechazados: " + rechazados);
+        Console.WriteLine("En revision: " + revision);
+
+       if (total > 0)
+        {
+            int porcentaje = (publicados * 100) / total;
+            Console.WriteLine("Porcentaje de aprobacion: " + porcentaje + "%");
+
+        }
+
+
+    }
+
+   
 
 }
